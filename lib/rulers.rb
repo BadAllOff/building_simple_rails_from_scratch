e8ed0df2c12f81ace5ede56325844fcf59ae89a9
Shamil <badalloffsh@gmail.com>
2017-06-26 16:11:25 +0400

    Add: root path

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,12 +1,37 @@
 # rulers/lib/rulers.rb
 require "rulers/version"
+require "rulers/routing"
 
 module Rulers
   class Application
     def call(env)
-      `echo debug > debug.txt`;
-      [200, {'Content-Type' => 'text/html'},
-       ["Hello from Ruby on Rulers!"]]
+
+      if env['PATH_INFO'] == '/favicon.ico'
+        return [404, {'Content-Type' => 'text/html'}, []]
+      elsif env['PATH_INFO'] == '/'
+        klass, act = [Object.const_get("HomeController"), 'index']
+        controller = klass.new(env)
+      else
+        klass, act = get_controller_and_action(env)
+        controller = klass.new(env)
+      end
+
+      begin
+        text = controller.send(act)
+        [200, {'Content-Type' => 'text/html'}, [text]]
+      rescue
+        [404, {'Content-Type' => 'text/html'},
+         ["404 man! Something went wrong..."]]
+      end
+    end
+  end
+
+  class Controller
+    def initialize(env)
+      @env = env
+    end
+    def env
+      @env
     end
   end
 end
